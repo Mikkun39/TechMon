c2b4377880b40d70cc37e7437b05aac4e73ee894
Mizuki Takayama <miz39t@gmail.com>
2020-09-11 19:24:20 +0900

    Initial Commit

---
+++ TechMonTests/TechMonTests.swift
@@ -0,0 +1,34 @@
+//
+//  TechMonTests.swift
+//  TechMonTests
+//
+//  Created by 高山瑞基 on 2020/09/11.
+//  Copyright © 2020 Takayama Mizuki. All rights reserved.
+//
+
+import XCTest
+@testable import TechMon
+
+class TechMonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
